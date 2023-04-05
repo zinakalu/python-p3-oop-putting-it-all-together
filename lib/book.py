765e40f9061b37cb8b6cd6bfedd67ac9737e7d94
zinakalu <zina.kalu@gmail.com>
2023-04-05 16:35:54 -0400

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+   
+   def __init__(self, title, page_count = 0):
+      self.title = title
+    #   self.author = author
+      self.page_count = page_count
+    #   self.genre = genre
+
+   def get_page_count(self):
+      return self._page_count
+   
+   def set_page_count(self, page_count):
+      if not isinstance(page_count,int):
+         print("page_count must be an integer")
+      else:
+         self._page_count = page_count
+
+   page_count = property(get_page_count, set_page_count)
+
+   #create page_count attribute and use property() to give page_count 
+   # the powers/abilities of the get or set methods depending on the context
+   #self.page-count will run the result of get_page_count()
+   #self.page_count = something will run the result of set_page_count()
+    #   else:
+    #      self._page_count = page_count
+   
+   def turn_page(self):
+        print ("Flipping the page...wow, you read fast!")
+
+
+new_book = Book("title of book", "566") 
+new_book
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, condition = "Used"):
+        self.brand = brand
+        # self.color = color
+        # self.size = size
+        # self.material = material
+        # self.condition = condition
+        self.condition = condition
+    _size = None
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer")
+        
+        else:
+            self._size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    size = property(get_size, set_size)
+shoes = Shoe("Nike")
